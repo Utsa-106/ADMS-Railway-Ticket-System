89ffa35650f89bcf7078a301fa1f82837585e10d
Utsa Podder <142795489+Utsa-106@users.noreply.github.com>
2026-06-12 02:23:50 +0600

    Update app.py

--- app.py
+++ app.py
@@ -265,5 +265,8 @@ def build_trip_advice(from_station, to_station, journey_date, train_class):
     }
 
 
-if __name__ == "__main__":
-    app.run(debug=True)
+import os
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
